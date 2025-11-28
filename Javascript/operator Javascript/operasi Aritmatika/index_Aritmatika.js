ec4a7ae52f400eeef7068aa87613a5168e8fb6d4
mancantak123 <nikosampit75@gmail.com>
2025-11-28 16:11:23 +0700

    refactor: reorganize and update JavaScript variable and arithmetic operation examples

---
+++ Javascript/operator Javascript/operasi Aritmatika/index_Aritmatika.js
@@ -0,0 +1,19 @@
+let a = 8;
+let b = 4;
+let hasil = a + b;//pertambahan 
+console.log(hasil)
+
+hasil = a - b; // perngurangan
+console.log(hasil)
+
+hasil = a * b; // perkalian
+console.log(hasil)
+
+hasil = a / b; //pembagian
+console.log(hasil)
+
+hasil = a % b; //module , hasil bagi
+console.log(hasil)
+
+hasil = a ** b; //pangkat
+console.log(hasil)
