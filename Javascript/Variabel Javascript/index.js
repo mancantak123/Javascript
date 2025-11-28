9ab1033dac7b90e12847951c0c6182f6cea4f736
mancantak123 <nikosampit75@gmail.com>
2025-11-28 15:37:19 +0700

    hello world

---
+++ Javascript/Variabel Javascript/index.js
@@ -0,0 +1,19 @@
+//ini adalah commet
+
+//variabel let
+
+let nama = "ucup serucup";
+console.log(nama)
+
+// mengubah value variabel
+nama = "udin";
+console.log(nama)
+
+//variabel const
+//variabel const tidak bisa diganti!!
+const umur = 20;
+console.log(umur)
+
+// var (tidak disarankan)
+var buah = "anggur";
+console.log(buah)
